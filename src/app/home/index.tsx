fd7c449f7d393d748adf9cad03fa9669a6c3854d
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-14 19:53:44 -0300

    add Header component on homepage :robot:

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,8 +1,9 @@
+import { Header } from "@/components/Header";
 
 export const HomePage = () => {
   return (
-    <div className="flex flex-col items-center w-1/1 h-screen bg-indigo-950 bg-landing bg-no-repeat bg-center">
-      <span>vapo</span>
+    <div className="flex flex-col items-center w-full h-screen">
+      <Header />
     </div>
   )
 };
