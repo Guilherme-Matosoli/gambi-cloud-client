6c4d8b72383e64a92b66d780af270ebe3d221ba8
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-14 17:23:41 -0300

    add light propertys on tailwind config :fire:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,8 +11,16 @@ const config: Config = {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-        "landing": "url('/backgroundImage.svg')"
       },
+      blur: {
+        "light": "calc(600px / 5)"
+      },
+      width: {
+        "light": "600px"
+      },
+      height: {
+        "light": "600px"
+      }
     },
   },
   plugins: [],
