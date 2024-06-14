7df531693c701c32130aa2afacafdb1d54aa6791
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-14 17:22:12 -0300

    add BlurLight component :tada:

---
+++ src/components/BlurLight/index.tsx
@@ -0,0 +1,13 @@
+
+interface BlueLightProps {
+  position?: string,
+  translate: string
+};
+
+export const BlurLight: React.FC<BlueLightProps> = ({ position, translate }) => {
+  return (
+    <div className={`absolute ${position} w-light h-light rounded-full bg-gradient-to-r from-indigo-400 to-indigo-800 opacity-50 filter blur-light ${translate}`}>
+
+    </div>
+  )
+};
