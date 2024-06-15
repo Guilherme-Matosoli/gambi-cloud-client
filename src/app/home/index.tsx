8036548bc356e1ab7f5e026bfdad722fb7515a98
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-14 23:11:01 -0300

    structured and stylish homepage :robot:

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,9 +1,38 @@
 import { Header } from "@/components/Header";
+import Link from "next/link";
 
 export const HomePage = () => {
   return (
     <div className="flex flex-col items-center w-full h-screen">
       <Header />
+      <main className="flex flex-col items-center gap-10 w-3/4 h-full pt-10 z-10">
+        <span className="font-montserrat text-white px-8 py-2 rounded-full border border-white">
+          Seja bem vindo à GambiCloud
+        </span>
+
+        <h2 className="w-full text-center font-montserrat font-bold text-white text-7xl gradient-text">
+          Mais que um simples armazenamento em núvem
+        </h2>
+
+        <section className="w-full flex flex-col gap-5 items-center text-gray-300">
+          <p>Armazene e compartilhe suas imagens diariamente com seus amigos</p>
+          <Link href="" className="font-montserrat font-medium text-xl bg-violet-900 cursor-pointer py-3 px-20 rounded-full transition-all hover:brightness-110">
+            COMEÇAR
+          </Link>
+        </section>
+
+        <img
+          src="/cloud.svg"
+          alt="Núvem com uma chave ingles"
+          className="w-1/3 image-drop"
+          draggable={false}
+        />
+      </main>
+      <footer className="w-3/4 flex items-center justify-center">
+        <span className="text-white font-montserrat text-l">
+          &copy; {new Date().getFullYear()} Todos direitos reservados
+        </span>
+      </footer>
     </div>
   )
 };
