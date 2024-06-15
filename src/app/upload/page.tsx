ef51cf62b4e4615e34b74cfa5bb2b11c061d97af
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-15 18:02:17 -0300

    add H2 and file input on upload page :fire:

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,8 +1,15 @@
+import { FileInput } from "@/components/FileInput";
 
 const Upload = () => {
   return (
-    <main>
-      hello world
+    <main className="flex items-center justify-center w-full h-screen z-10">
+      <div className="flex flex-col items-center w-2/3 h-auto">
+        <h2 className="font-montserrat gradient-text text-5xl font-bold">
+          Digite sua hash:
+        </h2>
+
+        <FileInput />
+      </div>
     </main>
   );
 };
