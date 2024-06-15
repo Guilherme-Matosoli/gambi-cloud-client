cfa9435cda54a2669d3725a4296b7c6277dcbbcd
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-14 23:39:30 -0300

    add an space between homepage  main and footer in mobile devies :iphone:

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -30,7 +30,7 @@ export const HomePage = () => {
           draggable={false}
         />
       </main>
-      <footer className="w-3/4 flex items-center justify-center z-10">
+      <footer className="w-3/4 flex items-center justify-center z-10 max-mobile:mt-5">
         <span className="text-white font-montserrat text-l max-mobile:text-xs">
           &copy; {new Date().getFullYear()} Todos direitos reservados
         </span>
