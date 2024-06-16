52ea84eaa3bfb0348e0d6c9c5ee499a3fc39e695
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 18:31:08 -0300

    add an loading icon :tada:

---
+++ src/components/LoadingIcon/index.tsx
@@ -0,0 +1,10 @@
+
+export const LoadingIcon = () => {
+  return (
+    <div className="-translate-y-1/2 -translate-x-1/2 z-0 absolute top-1/2 left-1/2">
+      <div className="border-2 border-gray-500 border-t-white rounded-full p-3 animate-spin">
+
+      </div>
+    </div>
+  );
+};
