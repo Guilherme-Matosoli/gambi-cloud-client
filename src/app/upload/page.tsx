2ef780244fe4ece55e5a63613e251d664851a87b
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 00:03:57 -0300

    add randomHash function and encapsulate content in a form :tada:

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -5,6 +5,7 @@ import { DragEvent, useState } from "react";
 
 const Upload = () => {
   const [image, setImage] = useState<FileList>();
+  const [hash, setHash] = useState<string>("");
 
   const handleDrop = (e: DragEvent<HTMLLabelElement>) => {
     e.preventDefault();
@@ -14,16 +15,35 @@ const Upload = () => {
     console.log(image)
   };
 
+  const randomizeHash = () => {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+
+    for (let i = 0; i < 20; i++) {
+      result += characters.charAt(Math.floor(Math.random() * characters.length))
+    };
+
+    setHash(result);
+  };
+
   return (
     <main className="flex items-center justify-center w-full h-screen z-10">
-      <div className="flex flex-col items-center w-auto h-auto">
-        <h2 className="font-montserrat gradient-text text-5xl font-bold">
-          Digite sua hash:
-        </h2>
+      <div className="flex justify-center-center w-auto h-auto">
+        <form className="flex flex-col items-center">
+          <h2 className="font-montserrat gradient-text text-5xl font-bold">
+            Digite sua hash:
+          </h2>
 
-        <Input />
+          <Input
+            placeholder="Digite sua hash"
+            onClickButton={randomizeHash}
+            value={hash}
+            onChange={e => setHash(e.target.value)}
+            required
+          />
 
-        <FileInput onDropFunc={handleDrop} />
+          <FileInput onDropFunc={handleDrop} />
+        </form>
       </div>
     </main>
   );
