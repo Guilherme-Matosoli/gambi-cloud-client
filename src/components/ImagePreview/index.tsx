32c6e3a837cd628b20bd8a9e8529709c20d28d21
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 17:35:03 -0300

    add drop-shadow in image preview :rocket:

--- src/components/ImagePreview/index.tsx
+++ src/components/ImagePreview/index.tsx
@@ -7,7 +7,7 @@ interface ImagePreviewProps {
 
 export const ImagePreview: React.FC<ImagePreviewProps> = ({ imageSrc, imageName, onClick }) => {
   return (
-    <section className="flex items-center relative justify-center w-80 h-52 p-2 border border-white rounded-lg">
+    <section className="flex items-center relative justify-center w-80 h-52 p-2 border border-white rounded-lg image-drop">
       <button onClick={onClick} className="absolute top-0 right-0 rounded-full p-1 bg-red-500 transition-smooth hover:scale-110">
         <img
           src="/trashIcon.svg"
