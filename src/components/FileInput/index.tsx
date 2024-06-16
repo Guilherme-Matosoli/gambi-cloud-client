c4fe60b8597c771e8865f7c0ecedadf7a154987f
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-15 22:09:28 -0300

    add drag&drop functionality :robot:

--- src/components/FileInput/index.tsx
+++ src/components/FileInput/index.tsx
@@ -1,17 +1,24 @@
-import { InputHTMLAttributes } from "react";
+"use client";
+import { DragEvent, InputHTMLAttributes } from "react";
 
 interface FileInputProps extends InputHTMLAttributes<HTMLInputElement> {
-
+  onDropFunc: (e: DragEvent<HTMLLabelElement>) => void;
 };
 
-export const FileInput: React.FC<FileInputProps> = ({ ...rest }) => {
+export const FileInput: React.FC<FileInputProps> = ({ onDropFunc, ...rest }) => {
 
   return (
-    <label htmlFor="image" className="cursor-pointer flex items-center justify-center w-80 h-52 bg-indigo-800 rounded-xl gradient-background">
-      <input id="image" type="file" hidden {...rest} />
+    <label onDragOver={e => e.preventDefault()} onDrop={e => onDropFunc(e)} htmlFor="image" className="cursor-pointer flex flex-col gap-5 items-center justify-center w-80 h-52 bg-indigo-800 rounded-xl gradient-background transition-smooth hover-input">
+      <input id="image" type="file" hidden accept="image/*"{...rest} />
+
       <span className="text-white font-montserrat font-medium">
         Selecione sua imagem
       </span>
+
+      <img
+        src="/uploadIcon.svg"
+        alt="Cloud that indicates upload"
+      />
     </label>
   )
 };
