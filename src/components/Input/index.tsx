5495a993bd670cb1b8099953082e6045844a3f04
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 00:19:19 -0300

    change Input placeholder color because white looks like value :hammer:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -7,7 +7,7 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 export const Input: React.FC<InputProps> = ({ onClickButton, ...rest }) => {
   return (
     <section className="w-full flex justify-center gradient-background border border-indigo-200 rounded-md input-focus transition-smooth">
-      <input {...rest} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-white font-montserrat" />
+      <input {...rest} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-gray-400 font-montserrat" />
       <button type="button" className="group px-2" onClick={onClickButton}>
         <img
           src="/refreshIcon.svg"
