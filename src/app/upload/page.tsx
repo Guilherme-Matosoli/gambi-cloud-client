0e5d0c2f4ae1e3baf1618e9ae67e6b236f1f14f8
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-15 22:09:51 -0300

    add handle drop function to setImage :tada:

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,6 +1,18 @@
+"use client";
 import { FileInput } from "@/components/FileInput";
+import { DragEvent, useState } from "react";
 
 const Upload = () => {
+  const [image, setImage] = useState<FileList>();
+
+  const handleDrop = (e: DragEvent<HTMLLabelElement>) => {
+    e.preventDefault();
+    setImage(e.dataTransfer.files);
+
+
+    console.log(image)
+  };
+
   return (
     <main className="flex items-center justify-center w-full h-screen z-10">
       <div className="flex flex-col items-center w-2/3 h-auto">
@@ -8,7 +20,7 @@ const Upload = () => {
           Digite sua hash:
         </h2>
 
-        <FileInput />
+        <FileInput onDropFunc={handleDrop} />
       </div>
     </main>
   );
