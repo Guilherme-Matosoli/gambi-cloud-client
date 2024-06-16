993fcc2452f2a6c7fd2a7d730f18e1980a1e7209
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-15 23:43:51 -0300

    structured input and random hash button :robot:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -2,8 +2,15 @@
 
 export const Input = () => {
   return (
-    <section>
-
+    <section className="w-full flex justify-center gradient-background border border-indigo-200 rounded-md input-focus transition-smooth">
+      <input placeholder="Digite a hash" className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-white font-montserrat" />
+      <button className="group px-2">
+        <img
+          src="/refreshIcon.svg"
+          alt="Refresh"
+          className="transition-smooth group-hover:scale-110"
+        />
+      </button>
     </section>
   );
 };
