a970435c892b86fd20b9a026b2aba37a19d199c8
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 00:04:19 -0300

    add InputProps interface :fire:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,10 +1,14 @@
+import { InputHTMLAttributes } from "react";
 
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  onClickButton: () => void
+};
 
-export const Input = () => {
+export const Input: React.FC<InputProps> = ({ onClickButton, ...rest }) => {
   return (
     <section className="w-full flex justify-center gradient-background border border-indigo-200 rounded-md input-focus transition-smooth">
-      <input placeholder="Digite a hash" className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-white font-montserrat" />
-      <button className="group px-2">
+      <input {...rest} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-white font-montserrat" />
+      <button className="group px-2" onClick={onClickButton}>
         <img
           src="/refreshIcon.svg"
           alt="Refresh"
