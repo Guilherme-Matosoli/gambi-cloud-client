ad4fbf54acf3b07a69c2a77d59f98e457a7d3208
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 00:06:07 -0300

    add type button because default has resulting in form submit :bug:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -8,7 +8,7 @@ export const Input: React.FC<InputProps> = ({ onClickButton, ...rest }) => {
   return (
     <section className="w-full flex justify-center gradient-background border border-indigo-200 rounded-md input-focus transition-smooth">
       <input {...rest} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-white font-montserrat" />
-      <button className="group px-2" onClick={onClickButton}>
+      <button type="button" className="group px-2" onClick={onClickButton}>
         <img
           src="/refreshIcon.svg"
           alt="Refresh"
