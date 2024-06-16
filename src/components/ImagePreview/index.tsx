fa1e31bae12cf3c7ebf36cdc668de1b55ee86b34
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 18:31:39 -0300

    appear loading icon while image doesn't :boom:

--- src/components/ImagePreview/index.tsx
+++ src/components/ImagePreview/index.tsx
@@ -1,3 +1,4 @@
+import { LoadingIcon } from "../LoadingIcon";
 
 interface ImagePreviewProps {
   imageSrc?: string,
@@ -8,7 +9,7 @@ interface ImagePreviewProps {
 export const ImagePreview: React.FC<ImagePreviewProps> = ({ imageSrc, imageName, onClick }) => {
   return (
     <section className="flex items-center relative justify-center w-80 h-52 p-2 border border-white rounded-lg image-drop">
-      <button onClick={onClick} className="absolute top-0 right-0 rounded-full p-1 bg-red-500 transition-smooth hover:scale-110">
+      <button onClick={onClick} className="z-20 absolute top-0 right-0 rounded-full p-1 bg-fuchsia-600 transition-smooth hover:scale-110">
         <img
           src="/trashIcon.svg"
         />
@@ -17,7 +18,10 @@ export const ImagePreview: React.FC<ImagePreviewProps> = ({ imageSrc, imageName,
       <img
         src={imageSrc}
         alt={imageName}
+        className="z-10"
       />
+
+      <LoadingIcon />
     </section>
   )
 };
