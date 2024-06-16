0c70b4f917dbd25257e913aa7bc2b11693420318
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 19:24:04 -0300

    add responsive design to FileInput :iphone:

--- src/components/FileInput/index.tsx
+++ src/components/FileInput/index.tsx
@@ -8,16 +8,17 @@ interface FileInputProps extends InputHTMLAttributes<HTMLInputElement> {
 export const FileInput: React.FC<FileInputProps> = ({ onDropFunc, ...rest }) => {
 
   return (
-    <label onDragOver={e => e.preventDefault()} onDrop={e => onDropFunc(e)} htmlFor="image" className="cursor-pointer flex flex-col gap-5 items-center justify-center w-80 h-52 bg-indigo-800 rounded-xl gradient-background transition-smooth hover-input">
+    <label onDragOver={e => e.preventDefault()} onDrop={e => onDropFunc(e)} htmlFor="image" className="cursor-pointer flex flex-col gap-5 items-center justify-center w-80 h-52 bg-indigo-800 rounded-xl gradient-background transition-smooth hover-input max-phone:h-28 max-phone:w-56 max-phone:p-5 max-phone:gap-1">
       <input id="image" type="file" hidden accept="image/*"{...rest} />
 
-      <span className="text-white font-montserrat font-medium">
+      <span className="text-white font-montserrat font-medium text-center">
         Selecione sua imagem
       </span>
 
       <img
         src="/uploadIcon.svg"
         alt="Cloud that indicates upload"
+        className="max-phone: w-10"
       />
     </label>
   )
