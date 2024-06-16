935adc7af55dd01964a1552e8ee722ad5bd6262f
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-15 21:39:59 -0300

    add new transition delay (smooth) :fire:

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,6 +33,9 @@ const config: Config = {
       screens: {
         "mobile": "900px",
         "phone": "600px"
+      },
+      transitionProperty: {
+        "smooth": ".3s"
       }
     },
   },
