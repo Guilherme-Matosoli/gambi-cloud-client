50d3aed7ae2bf1189f8c29169c124299513c0ca9
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 20:17:17 -0300

    implements function to send files to cloud :fire:

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -3,23 +3,23 @@ import { FileInput } from "@/components/FileInput";
 import { Header } from "@/components/Header";
 import { ImagePreview } from "@/components/ImagePreview";
 import { Input } from "@/components/Input";
-import { DragEvent, useState } from "react";
+import { LoadingIcon } from "@/components/LoadingIcon";
+import { api } from "@/services/api";
+import { DragEvent, FormEvent, useState } from "react";
 
 const Upload = () => {
-  const [image, setImage] = useState<FileList | null>();
+  const [image, setImage] = useState<File | null>();
   const [hash, setHash] = useState<string>("");
 
   const handleDrop = (e: DragEvent<HTMLLabelElement>) => {
     e.preventDefault();
-    setImage(e.dataTransfer.files);
 
-
-    console.log(image)
+    if (e.dataTransfer.files[0]) setImage(e.dataTransfer.files[0]);
   };
 
   const randomizeHash = () => {
     let result = '';
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+-=[]{}|;:,.<>?';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
     for (let i = 0; i < 50; i++) {
       result += characters.charAt(Math.floor(Math.random() * characters.length))
@@ -28,11 +28,32 @@ const Upload = () => {
     setHash(result);
   };
 
+  const [promise, setPromise] = useState(true);
+  const handleUpload = async (e: FormEvent) => {
+    setPromise(true);
+    e.preventDefault();
+    try {
+      if (!image) return;
+
+      const formData = new FormData();
+      formData.append('file', image);
+      const response = await api.post(`/upload/${hash}`, formData);
+
+      console.log(response)
+    }
+    catch (err) {
+      console.log(err);
+    }
+    finally {
+      setPromise(false)
+    };
+  };
+
   return (
     <main className="flex flex-col items-center justify-between w-full h-screen">
       <Header />
       <div className="flex flex-1 justify-center items-center w-auto h-auto z-10 max-phone: pb-3">
-        <form className="flex flex-col items-center gap-10 max-mobile:px-5">
+        <form onSubmit={handleUpload} className="flex flex-col items-center gap-10 max-mobile:px-5">
           <div className="flex flex-col items-center gap-2">
             <h2 className="font-montserrat gradient-text text-5xl font-bold text-center leading-snug">
               Digite sua hash:
@@ -54,13 +75,15 @@ const Upload = () => {
           {
             image
               ?
-              <ImagePreview imageSrc={URL.createObjectURL(image[0])} imageName={image[0].name} onClick={() => setImage(null)} />
+              <ImagePreview imageSrc={URL.createObjectURL(image)} imageName={image.name} onClick={() => setImage(null)} />
               :
-              <FileInput onDropFunc={handleDrop} onChange={e => setImage(e.target.files)} />
+              <FileInput onDropFunc={handleDrop} onChange={e => e.target.files && setImage(e.target.files[0])} />
           }
 
-          <button className="button">
-            Upload
+          <button className="button relative">
+            {
+              promise ? <LoadingIcon /> : 'Upload'
+            }
           </button>
         </form>
       </div>
