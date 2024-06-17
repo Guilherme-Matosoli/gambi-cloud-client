427c5113b9c60f07a3672f42b428b4353ceb7c55
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-17 13:34:01 -0300

    add function to get image list :fire:

--- src/app/galery/[hash]/page.tsx
+++ src/app/galery/[hash]/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+import { api } from "@/services/api";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 interface ListImageParams {
   params: {
@@ -6,9 +9,35 @@ interface ListImageParams {
   }
 };
 
+interface Image {
+  filename: string
+};
+
 const ListImage: NextPage<ListImageParams> = ({ params: { hash } }) => {
+  const [listImage, setListImage] = useState<Image[]>([]);
+
+  const getImages = async () => {
+    try {
+      const response = await api.get(`/images/${hash}`);
+      setListImage(response.data);
+    }
+    catch (err) {
+      console.log(err);
+    };
+  };
+
+  useEffect(() => {
+    getImages();
+  }, [hash]);
+
   return (
-    <p>hash: {hash}</p>
+    <p>
+      hash: {hash}
+
+      <img
+        src={`http://localhost:4000/render/${hash}/${listImage[0]?.filename}`}
+      />
+    </p>
   )
 };
 
