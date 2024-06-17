67b586de29037e252f6eeae59baad1fed75492d7
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-17 00:19:02 -0300

    initialize galery page :fire:

---
+++ src/app/galery/page.tsx
@@ -0,0 +1,15 @@
+import { Footer } from "@/components/Footer";
+import { Header } from "@/components/Header";
+
+
+const Galery = () => {
+  return (
+    <main className="flex w-full h-screen flex-col items-center justify-between">
+      <Header />
+      <Footer />
+    </main>
+  )
+};
+
+
+export default Galery;
