47c0ebfc8fbe99585743b0faa45d87e361a90b89
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-17 00:13:03 -0300

    add warn to image size (max 2MB) :tada:

--- src/components/FileInput/index.tsx
+++ src/components/FileInput/index.tsx
@@ -11,9 +11,15 @@ export const FileInput: React.FC<FileInputProps> = ({ onDropFunc, ...rest }) => 
     <label onDragOver={e => e.preventDefault()} onDrop={e => onDropFunc(e)} htmlFor="image" className="cursor-pointer flex flex-col gap-5 items-center justify-center w-80 h-52 bg-indigo-800 rounded-xl gradient-background transition-smooth hover-input max-phone:h-28 max-phone:w-56 max-phone:p-5 max-phone:gap-1">
       <input id="image" type="file" hidden accept="image/*"{...rest} />
 
-      <span className="text-white font-montserrat font-medium text-center">
-        Selecione sua imagem
-      </span>
+      <div className="flex flex-col items-center">
+        <span className="text-white font-montserrat font-medium text-center max-phone:text-xs">
+          Selecione sua imagem
+        </span>
+
+        <p className="text-red-200 font-montserrat text-xs max-phone:text-mini">
+          ( Tamanho máximo de 2Mb )
+        </p>
+      </div>
 
       <img
         src="/uploadIcon.svg"
