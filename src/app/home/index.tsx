5c4a294b01623af4b80be67af76d1e38af4df4b8
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-16 21:03:48 -0300

    remove brute footer and replace to footer component :robot:

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 import Link from "next/link";
 
@@ -30,11 +31,7 @@ export const HomePage = () => {
           draggable={false}
         />
       </main>
-      <footer className="w-3/4 flex items-center justify-center z-10 max-mobile:mt-5">
-        <span className="text-white font-montserrat text-l max-mobile:text-xs">
-          &copy; {new Date().getFullYear()} Todos direitos reservados
-        </span>
-      </footer>
+      <Footer />
     </div>
   )
 };
