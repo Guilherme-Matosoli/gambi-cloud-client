ae12af81461b1528342d58419cef1066b08ae237
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-17 12:43:53 -0300

    add hiddenButton prop to hidden button when necessary :tada:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,14 +1,15 @@
 import { InputHTMLAttributes } from "react";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-  onClickButton: () => void
+  onClickButton?: () => void,
+  hiddenButton?: boolean
 };
 
-export const Input: React.FC<InputProps> = ({ onClickButton, ...rest }) => {
+export const Input: React.FC<InputProps> = ({ onClickButton, hiddenButton, ...rest }) => {
   return (
     <section className="w-full flex justify-center gradient-background border border-indigo-200 rounded-md input-focus transition-smooth">
       <input {...rest} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-gray-400 font-montserrat" />
-      <button type="button" className="group px-2" onClick={onClickButton}>
+      <button type="button" className="group px-2" onClick={onClickButton} hidden={hiddenButton}>
         <img
           src="/refreshIcon.svg"
           alt="Refresh"
