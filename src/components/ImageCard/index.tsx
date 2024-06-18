22eef899c28aed1bdc54e8e86b6ee93ed4c37158
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 15:59:44 -0300

    add more width to imagecard and add full width to download button :iphone:

--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -7,7 +7,7 @@ export const ImageCard: React.FC<ImageProps> = ({ filename, hash }) => {
   const imageLink = `${process.env.NEXT_PUBLIC_API_URL}/render/${hash}/${filename}`
 
   return (
-    <div className="w-40 h-44 flex flex-col items-center justify-between p-2 bg-violet-900 rounded-xl border border-white">
+    <div className="w-60 h-52 flex flex-col items-center justify-between p-2 bg-violet-900 rounded-xl border border-white">
       <img
         src={imageLink}
         alt={filename}
@@ -18,7 +18,7 @@ export const ImageCard: React.FC<ImageProps> = ({ filename, hash }) => {
         {filename}
       </span>
 
-      <a href={imageLink} download={filename} className="rounded-full p-2 bg-violet-500 transition-smooth hover:brightness-110 border border-white">
+      <a href={imageLink} download={filename} className="w-full flex items-center justify-center rounded-xl p-2 bg-violet-500 transition-smooth hover:brightness-110 border border-white">
         <img
           src="/downloadIcon.svg"
         />
