96cb686402b50a33790d42d53478d5789ef41d95
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 17:31:59 -0300

    add an image preview in screen size :rocket:

--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ImageModal } from "../ImageModal";
+
 interface ImageProps {
   filename?: string,
   hash?: string
@@ -5,13 +8,16 @@ interface ImageProps {
 
 export const ImageCard: React.FC<ImageProps> = ({ filename, hash }) => {
   const imageLink = `${process.env.NEXT_PUBLIC_API_URL}/render/${hash}/${filename}`
+  const [visible, setVisible] = useState(false);
 
   return (
     <div className="w-60 min-h-52 flex flex-col items-center justify-between p-2 bg-violet-900 rounded-xl border border-white max-phone:w-full">
+      <ImageModal imageSrc={imageLink} visible={visible} handleClose={() => setVisible(false)} />
       <img
         src={imageLink}
         alt={filename}
-        className="rounded-xl bg-white border border-white"
+        className="rounded-xl cursor-pointer bg-white border border-white transition-smooth hover-input"
+        onClick={() => setVisible(true)}
       />
 
       <span className="font-montserrat text-white text-xs">
--- src/components/ImageModal/index.tsx
+++ src/components/ImageModal/index.tsx
@@ -1,17 +1,19 @@
 interface ImageModalProps {
-  imageSrc: string
+  imageSrc: string,
+  visible: boolean,
+  handleClose: () => void
 };
 
 
-export const ImageModal: React.FC<ImageModalProps> = ({ imageSrc }) => {
-  const handleClose = (e: React.MouseEvent<HTMLElement>) => {
+export const ImageModal: React.FC<ImageModalProps> = ({ imageSrc, visible, handleClose }) => {
+  const handleCloseFunc = (e: React.MouseEvent<HTMLElement>) => {
     if (e.target instanceof HTMLImageElement) return;
 
-    console.log("clicked")
+    handleClose();
   };
 
-  return (
-    <section onClick={e => handleClose(e)} className="flex items-center justify-center fixed top-0 w-screen h-screen bg-black/[.9] z-30 max-mobile:px-3">
+  return visible && (
+    <section onClick={e => handleCloseFunc(e)} className="flex items-center justify-center fixed top-0 w-screen h-screen bg-black/[.9] z-30 max-mobile:px-3">
       <div className="w-2/3 bg-white rounded-xl overflow-hidden border-2 border-white max-mobile:w-full">
         <img
           src={imageSrc}
