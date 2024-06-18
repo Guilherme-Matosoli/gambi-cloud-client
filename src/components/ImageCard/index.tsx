296087ff3734307573f82212e8cb2bc6e2d580ee
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-17 21:30:44 -0300

    remove unecessary line break :fire:

--- src/components/ImageCard/index.tsx
+++ src/components/ImageCard/index.tsx
@@ -1,4 +1,3 @@
-
 interface ImageProps {
   filename?: string,
   hash?: string
@@ -8,18 +7,18 @@ export const ImageCard: React.FC<ImageProps> = ({ filename, hash }) => {
   const imageLink = `${process.env.NEXT_PUBLIC_API_URL}/render/${hash}/${filename}`
 
   return (
-    <div className="w-40 h-44 flex flex-col items-center justify-between p-2 bg-violet-900 rounded-xl">
+    <div className="w-40 h-44 flex flex-col items-center justify-between p-2 bg-violet-900 rounded-xl border border-white">
       <img
         src={imageLink}
         alt={filename}
-        className="rounded-xl bg-white"
+        className="rounded-xl bg-white border border-white"
       />
 
       <span className="font-montserrat text-white text-xs">
         {filename}
       </span>
 
-      <a href={imageLink} download={filename} className="rounded-full p-2 bg-violet-500 transition-smooth hover:brightness-110">
+      <a href={imageLink} download={filename} className="rounded-full p-2 bg-violet-500 transition-smooth hover:brightness-110 border border-white">
         <img
           src="/downloadIcon.svg"
         />
