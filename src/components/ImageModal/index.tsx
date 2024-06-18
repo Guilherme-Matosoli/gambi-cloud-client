02d3f9eeb23b65cce1f00440f205eab370759655
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 19:14:15 -0300

    solve ImageModal position bug :bug:

--- src/components/ImageModal/index.tsx
+++ src/components/ImageModal/index.tsx
@@ -13,7 +13,7 @@ export const ImageModal: React.FC<ImageModalProps> = ({ imageSrc, visible, handl
   };
 
   return visible && (
-    <section onClick={e => handleCloseFunc(e)} className="flex items-center justify-center fixed top-0 w-screen h-screen bg-black/[.9] z-30 max-mobile:px-3">
+    <section onClick={e => handleCloseFunc(e)} className="flex items-center justify-center fixed right-0 top-0 w-screen h-screen bg-black/[.9] z-30 max-mobile:px-3">
       <div className="w-2/3 bg-white rounded-xl overflow-hidden border-2 border-white max-mobile:w-full">
         <img
           src={imageSrc}
