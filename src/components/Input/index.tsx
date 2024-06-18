42990623fc0d2aa1f11e0bf5260e6ff91eba7126
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 18:51:21 -0300

    add Copy button on Input component :iphone:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -9,10 +9,19 @@ export const Input: React.FC<InputProps> = ({ onClickButton, hiddenButton, ...re
   return (
     <section className="w-full flex justify-center gradient-background border border-indigo-200 rounded-md input-focus transition-smooth">
       <input {...rest} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-gray-400 font-montserrat" />
+
       <button type="button" className="group px-2" onClick={onClickButton} hidden={hiddenButton}>
         <img
           src="/refreshIcon.svg"
           alt="Refresh"
+          className="transition-smooth group-hover:scale-110"
+        />
+      </button>
+
+      <button type="button" className="group px-2" onClick={onClickButton}>
+        <img
+          src="/copyIcon.svg"
+          alt="Copy"
           className="transition-smooth group-hover:scale-110"
         />
       </button>
