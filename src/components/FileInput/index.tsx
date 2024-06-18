0c3fda85964426eb28585501a2d632b788f2e4e8
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 16:53:11 -0300

    validate image here and error message will appear on the top :tada:

--- src/components/FileInput/index.tsx
+++ src/components/FileInput/index.tsx
@@ -1,15 +1,39 @@
 "use client";
-import { DragEvent, InputHTMLAttributes } from "react";
+import { validateImage } from "@/utils/validateImage";
+import { ChangeEvent, DragEvent, InputHTMLAttributes, useState } from "react";
 
 interface FileInputProps extends InputHTMLAttributes<HTMLInputElement> {
-  onDropFunc: (e: DragEvent<HTMLLabelElement>) => void;
+  onDropFunc: (e: DragEvent<HTMLLabelElement>) => void,
+  onChangeFunc: (e: ChangeEvent<HTMLInputElement>) => void,
 };
 
-export const FileInput: React.FC<FileInputProps> = ({ onDropFunc, ...rest }) => {
+export const FileInput: React.FC<FileInputProps> = ({ onDropFunc, onChangeFunc, ...rest }) => {
+  const [error, setError] = useState<string | null>(null);
 
   return (
-    <label onDragOver={e => e.preventDefault()} onDrop={e => onDropFunc(e)} htmlFor="image" className="cursor-pointer flex flex-col gap-5 items-center justify-center w-80 h-52 bg-indigo-800 rounded-xl gradient-background transition-smooth hover-input max-phone:h-28 max-phone:w-56 max-phone:p-5 max-phone:gap-1">
-      <input id="image" type="file" hidden accept="image/*"{...rest} />
+    <label onDragOver={e => e.preventDefault()} onDrop={e => onDropFunc(e)} htmlFor="image" className="relative cursor-pointer flex flex-col gap-5 items-center justify-center w-80 h-52 bg-indigo-800 rounded-xl gradient-background transition-smooth hover-input max-phone:h-28 max-phone:w-56 max-phone:p-5 max-phone:gap-1">
+      <span className="font-montserrat text-red-500 absolute top-0 -translate-y-full">{error}</span>
+
+      <input
+        id="image"
+        type="file"
+        hidden
+        accept="image/*"{...rest}
+        onChange={e => {
+          const validate = e.target.files && validateImage(e.target.files[0]);
+          if (validate == "format") {
+            setError("Formato de arquivo não aceito")
+            return;
+          };
+
+          if (validate == "size") {
+            setError("Tamanho do arquivo não aceito (max 2Mb)")
+            return;
+          };
+
+          onChangeFunc(e);
+        }}
+      />
 
       <div className="flex flex-col items-center">
         <span className="text-white font-montserrat font-medium text-center max-phone:text-xs">
