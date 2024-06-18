dbb368cc064a6effb3d966fc904ed252a09d2518
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-18 19:07:47 -0300

    add copy function :fire:

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,4 +1,5 @@
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, useRef } from "react";
+import { toast } from "react-toastify";
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   onClickButton?: () => void,
@@ -6,9 +7,18 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
 };
 
 export const Input: React.FC<InputProps> = ({ onClickButton, hiddenButton, ...rest }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const copyToClip = () => {
+    if (inputRef?.current?.value) {
+      navigator.clipboard.writeText(inputRef.current.value);
+      toast("Copiado para área de transferência")
+    };
+  };
+
   return (
     <section className="w-full flex justify-center gradient-background border border-indigo-200 rounded-md input-focus transition-smooth">
-      <input {...rest} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-gray-400 font-montserrat" />
+      <input {...rest} ref={inputRef} className="w-full px-2 outline-none py-2 px-2 bg-transparent text-white placeholder-gray-400 font-montserrat" />
 
       <button type="button" className="group px-2" onClick={onClickButton} hidden={hiddenButton}>
         <img
@@ -18,7 +28,7 @@ export const Input: React.FC<InputProps> = ({ onClickButton, hiddenButton, ...re
         />
       </button>
 
-      <button type="button" className="group px-2" onClick={onClickButton}>
+      <button type="button" className="group px-2" onClick={copyToClip}>
         <img
           src="/copyIcon.svg"
           alt="Copy"
