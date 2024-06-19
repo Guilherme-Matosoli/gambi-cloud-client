0e29d0b363ace356789eaf818071fed9f53412ea
Guilherme-Matosoli <guilhermematosgto2015@outlook.com>
2024-06-19 18:13:57 -0300

    change home design :tada:

--- src/app/home/index.tsx
+++ src/app/home/index.tsx
@@ -6,30 +6,31 @@ export const HomePage = () => {
   return (
     <div className="flex flex-col items-center w-full h-screen">
       <Header />
-      <main className="flex flex-col items-center gap-10 w-3/4 h-full pt-10 z-10 max-mobile:w-full">
+      <main className="flex flex-col justify-center items-center gap-8 w-3/4 h-full pt-10 z-10 max-mobile:w-full">
         <span className="font-montserrat text-white px-8 py-2 rounded-full border border-white">
           Seja bem vindo à GambiCloud
         </span>
 
-        <h2 className="w-full text-center font-montserrat font-bold text-white text-7xl break-words gradient-text max-mobile:text-5xl max-phone:text-3xl">
+        <h2 className="w-full text-center leading-snug font-montserrat font-bold text-white text-7xl break-words gradient-text max-mobile:text-5xl max-phone:text-3xl">
           Mais que um simples armazenamento em núvem
         </h2>
 
         <section className="w-full flex flex-col gap-5 items-center text-gray-300">
-          <p className="text-center max-phone:text-xs">
-            Armazene e compartilhe suas imagens diariamente com seus amigos
+          <p className="text-center w-2/3 break-words text-18 max-phone:text-xs max-mobile:w-full">
+            Compartilhe e armazene suas imagens diariamente com amigos! Nossa plataforma garante privacidade com imagens limpas automaticamente às 00:00 e compartilhamento via hash.
           </p>
-          <Link href="/upload" className="button">
-            COMEÇAR
-          </Link>
+
+          <div className="flex gap-5 mt-10 max-phone:flex-col max-phone:gap-2 max-phone:w-full max-phone:items-center">
+            <Link href="/upload" className="button">
+              COMEÇAR
+            </Link>
+
+            <a target="_blank" href="https://www.alertpix.live/guimatosoli" className="button-light">
+              DONATE
+            </a>
+          </div>
         </section>
 
-        <img
-          src="/cloud.svg"
-          alt="Núvem com uma chave ingles"
-          className="w-1/3 image-drop max-mobile:w-1/2"
-          draggable={false}
-        />
       </main>
       <Footer />
     </div>
